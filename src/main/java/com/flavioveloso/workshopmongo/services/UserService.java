501401be4e2773378a47f4062cdc40ec6d7f57b0
Flavio Veloso <flaviosv@live.com>
2025-01-23 23:49:03 -0300

    Obtendo um usuario por id

--- src/main/java/com/flavioveloso/workshopmongo/services/UserService.java
+++ src/main/java/com/flavioveloso/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.flavioveloso.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.flavioveloso.workshopmongo.domain.User;
 import com.flavioveloso.workshopmongo.repository.UserRepository;
+import com.flavioveloso.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -19,6 +21,9 @@ public class UserService {
 		
 	}
 
-	
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 
 }

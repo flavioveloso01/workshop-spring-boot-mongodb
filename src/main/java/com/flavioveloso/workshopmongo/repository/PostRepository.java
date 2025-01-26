0c530447d466e37c3625aa147048e3cb0bf6b664
Flavio Veloso <flaviosv@live.com>
2025-01-26 18:58:52 -0300

    Consulta simples com query methods

--- src/main/java/com/flavioveloso/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/flavioveloso/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.flavioveloso.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,9 @@ import com.flavioveloso.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
+	
 }
+
 	
-	
+	 
--- src/main/java/com/flavioveloso/workshopmongo/services/PostService.java
+++ src/main/java/com/flavioveloso/workshopmongo/services/PostService.java
@@ -1,12 +1,12 @@
 package com.flavioveloso.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.flavioveloso.workshopmongo.domain.Post;
-import com.flavioveloso.workshopmongo.domain.User;
 import com.flavioveloso.workshopmongo.repository.PostRepository;
 import com.flavioveloso.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +22,9 @@ public class PostService {
 		Optional<Post> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
+	}
 
 }
